c7ff1665e1090ae52027c497c8fdea210558ebad
Rakshanda <rakshanda.mahajan1101@gmail.com>
2020-03-30 17:02:54 +0530

    Lab changes

---
+++ FrontEnd/src/components/LabModule/StartLab.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './StartLab.css';
+class LabStartPage extends React.Component{
+    render(){
+        return(
+            <h6 className ="decorateheader" >CLICK A BUTTON TO GET STARTED</h6>
+        );
+    }
+}
+export default LabStartPage;
